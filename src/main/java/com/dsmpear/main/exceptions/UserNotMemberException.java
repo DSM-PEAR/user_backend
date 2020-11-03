8febe4bf94d399e166b4187ac4f227aa82e5d8c6
이승윤 <yyuunn17@naver.com>
2020-11-03 23:20:50 +0900

    modify exception

--- src/main/java/com/dsmpear/main/exceptions/UserNotMemberException.java
+++ src/main/java/com/dsmpear/main/exceptions/UserNotMemberException.java
@@ -1,7 +1,9 @@
 package com.dsmpear.main.exceptions;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN,reason = "User is Not a Member of the Team")
 public class UserNotMemberException extends RuntimeException {
-    public UserNotMemberException(){
-        super("User is Not Member!!");
-    }
 }
