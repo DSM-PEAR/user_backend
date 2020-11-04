e0074838058be28c77fafc6d0a2d25647c19a850
이승윤 <yyuunn17@naver.com>
2020-11-04 19:41:23 +0900

    add set member & delete member

--- src/main/java/com/dsmpear/main/service/member/MemberServiceImpl.java
+++ src/main/java/com/dsmpear/main/service/member/MemberServiceImpl.java
@@ -1,7 +1,14 @@
 package com.dsmpear.main.service.member;
 
+import com.dsmpear.main.entity.member.Member;
 import com.dsmpear.main.entity.member.MemberRepository;
+import com.dsmpear.main.entity.team.Team;
+import com.dsmpear.main.entity.team.TeamRepository;
+import com.dsmpear.main.entity.user.User;
+import com.dsmpear.main.entity.user.UserRepository;
+import com.dsmpear.main.exceptions.*;
 import com.dsmpear.main.payload.request.MemberRequest;
+import com.dsmpear.main.security.auth.AuthenticationFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,15 +16,43 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService{
 
+    private final AuthenticationFacade authenticationFacade;
+    private final UserRepository userRepository;
     private final MemberRepository memberRepository;
+    private final TeamRepository teamRepository;
 
     @Override
     public void setMember(MemberRequest memberRequest) {
+        User user=userRepository.findByEmail(authenticationFacade.getUserEmail())
+                .orElseThrow(UserNotFoundException::new);
 
+        Team team=teamRepository.findById(memberRequest.getTeamId())
+                .orElseThrow(TeamNotFoundException::new);
+
+        memberRepository.findByTeamIdAndUserEmail(team.getId(),user.getEmail())
+                .ifPresent(member -> {throw new MemberAlreadyIncludeException();
+                });
+
+        memberRepository.save(
+                Member.builder()
+                        .teamId(team.getId())
+                        .userEmail(user.getEmail())
+                .build()
+        );
     }
 
     @Override
     public void deleteMember(Integer userEmail) {
+        User user=userRepository.findByEmail(authenticationFacade.getUserEmail())
+                .orElseThrow(UserNotMemberException::new);
+        
+        Member member=memberRepository.findByUserEmail(user.getEmail())
+                .orElseThrow(MemberNotFoundException::new);
+
+        teamRepository.findById(member.getTeamId())
+                .orElseThrow(TeamNotFoundException::new);
+
+        memberRepository.delete(member);
 
     }
 }
