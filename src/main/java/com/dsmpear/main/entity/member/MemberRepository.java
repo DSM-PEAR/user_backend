ee3af13e181b2997e4ea14677361709e36e66184
이승윤 <yyuunn17@naver.com>
2020-11-04 19:24:32 +0900

    modify MemberRepository

--- src/main/java/com/dsmpear/main/entity/member/MemberRepository.java
+++ src/main/java/com/dsmpear/main/entity/member/MemberRepository.java
@@ -16,6 +16,6 @@ public interface MemberRepository extends CrudRepository<Member,Integer> {
     //팀 아이디로 팀 삭제하기
     void deleteAllByTeamId(Integer teamId);
     //보고서 아이디로 팀 삭제하기
-    void deleteAllByReportId(Integer reportId);
+    //void deleteAllByReportId(Integer reportId);
 
 }
