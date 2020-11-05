1bb1405d67b90b0712ea802d73e91665c17b1540
kjbin0420 <kjbin0420@naver.com>
2020-11-05 10:59:59 +0900

    [Fix] Exception

--- src/main/java/com/dsmpear/main/exceptions/InvalidEmailAddressException.java
+++ src/main/java/com/dsmpear/main/exceptions/InvalidEmailAddressException.java
@@ -1,5 +1,9 @@
 package com.dsmpear.main.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "Invalid Email Address")
 public class InvalidEmailAddressException extends RuntimeException {
     public InvalidEmailAddressException() {
         super("INVALID_EMAIL_ADDRESS");
