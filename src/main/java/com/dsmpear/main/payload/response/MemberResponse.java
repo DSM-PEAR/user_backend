521e1ae033b9b66c9654277f36e000b97d75d285
이승윤 <yyuunn17@naver.com>
2020-11-11 09:43:57 +0900

    add : memberResponse

--- src/main/java/com/dsmpear/main/payload/response/MemberResponse.java
+++ src/main/java/com/dsmpear/main/payload/response/MemberResponse.java
@@ -1,4 +1,15 @@
 package com.dsmpear.main.payload.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class MemberResponse {
+
+    private Integer memberId;
+
+    private String membeEmail;
+
+    private String memberName;
 }
