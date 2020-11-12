ac23b2fe885b32687465f1190c18a181b661326e
이승윤 <yyuunn17@naver.com>
2020-11-12 17:05:45 +0900

    Finish Notice API
    Oh YEAH

---
+++ src/main/java/com/dsmpear/main/payload/response/NoticeResponse.java
@@ -0,0 +1,18 @@
+package com.dsmpear.main.payload.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class NoticeResponse {
+
+    private String title;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+}
