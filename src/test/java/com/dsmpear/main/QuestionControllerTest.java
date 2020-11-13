9a114f88d620d1f33a73ca113c13ce20a4c564cd
이승윤 <yyuunn17@naver.com>
2020-11-13 22:44:13 +0900

    Test  Code Import Modification
    question error fix

--- src/test/java/com/dsmpear/main/QuestionControllerTest.java
+++ src/test/java/com/dsmpear/main/QuestionControllerTest.java
@@ -2,23 +2,26 @@ package com.dsmpear.main;
 
 import com.dsmpear.main.payload.request.QuestionRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class QuestionControllerTest {
+@ActiveProfiles("test")
+public class QuestionControllerTest {
 
 
     @Autowired
@@ -26,16 +29,20 @@ class QuestionControllerTest {
 
     private MockMvc mvc;
 
-    @BeforeEach
+    @Before
     public void setup() {
         mvc = MockMvcBuilders
-              .webAppContextSetup(context)
+                .webAppContextSetup(context)
                 .build();
     }
-    @Test
-    void test() throws Exception {
 
-        QuestionRequest request = new QuestionRequest("asd@asd.asd", "asdasdasds");
+    @Test
+    public void test() throws Exception {
+
+        String email="test@dsm.hs.kr";
+        String description="description";
+
+        QuestionRequest request = new QuestionRequest(email,description);
 
         mvc.perform(post("/question")
                 .content(new ObjectMapper()
