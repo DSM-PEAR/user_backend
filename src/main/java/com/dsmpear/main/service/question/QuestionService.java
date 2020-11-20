1228271d47192f4c18288631d600ab70efae1ac5
jhhong0509 <jhhong0509@naver.com>
2020-11-20 13:23:40 +0900

    update comment,report

--- src/main/java/com/dsmpear/main/service/question/QuestionService.java
+++
@@ -1,7 +0,0 @@
-package com.dsmpear.main.service.question;
-
-import com.dsmpear.main.payload.request.QuestionRequest;
-
-public interface QuestionService {
-    void inquiry(QuestionRequest questionRequest);
-}
