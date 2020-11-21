4d8b76185744ecb552802139562dc2ad5e3df872
Hong Jung hyeon <jhhong0509@naver.com>
2020-11-21 13:19:58 +0900

    [Fix] some codes

--- src/main/java/com/dsmpear/main/entity/refreshtoken/RefreshToken.java
+++ src/main/java/com/dsmpear/main/entity/refreshtoken/RefreshToken.java
@@ -4,36 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-<<<<<<< HEAD
-=======
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
-import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
->>>>>>> develop
 
 import java.io.Serializable;
 
-<<<<<<< HEAD
-@Entity
-@Table(name = "refresh_token")
-=======
 @RedisHash(value = "refresh_token")
->>>>>>> develop
 @NoArgsConstructor @AllArgsConstructor @Getter
 @Builder
 public class RefreshToken implements Serializable {
     @Id
     private String email;
 
-<<<<<<< HEAD
-    @Column(nullable = false)
-=======
     @Indexed
->>>>>>> develop
     private String refreshToken;
 
-    @Column(nullable = false)
     private Long refreshExp;
 
     public RefreshToken update(String refreshToken, Long refreshExp) {
