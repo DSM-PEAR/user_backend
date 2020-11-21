4e7186e352be965b17619710fdb131328aab9811
Hong Jung hyeon <jhhong0509@naver.com>
2020-11-21 15:42:16 +0900

    [FIX]reportRepository

--- src/main/java/com/dsmpear/main/entity/report/ReportRepository.java
+++ src/main/java/com/dsmpear/main/entity/report/ReportRepository.java
@@ -1,12 +1,17 @@
 package com.dsmpear.main.entity.report;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+import java.util.List;
 import java.util.Optional;
 
 
 @Repository
 public interface ReportRepository extends CrudRepository<Report,Integer> {
-    Optional<Report> findById(Integer id);
+    Optional<Report> findByReportId(Integer reportId);
+    List<Report> findByAccess(Access access);
+    Page<Report> findAllBy(Pageable page);
 }
