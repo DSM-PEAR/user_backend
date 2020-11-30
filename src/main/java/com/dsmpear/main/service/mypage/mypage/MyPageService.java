90ea385fab6d11466560e807a64878a5e98aea2c
이승윤 <yyuunn17@naver.com>
2020-11-30 15:00:32 +0900

    delete profile

--- src/main/java/com/dsmpear/main/service/mypage/mypage/MyPageService.java
+++
@@ -1,8 +0,0 @@
-package com.dsmpear.main.service.mypage.mypage;
-
-import com.dsmpear.main.payload.response.ProfilePageResponse;
-
-public interface MyPageService {
-    ProfilePageResponse getMyPage();
-    void setSelfIntro(String intro, String gitHub);
-}
