2f025a40c6cd4a5bcee94df0592830b15c64ab4a
Hong Jung hyeon <jhhong0509@naver.com>
2020-12-13 22:16:00 +0900

    bug fix

--- src/main/java/com/dsmpear/main/entity/report/ReportRepository.java
+++ src/main/java/com/dsmpear/main/entity/report/ReportRepository.java
@@ -12,5 +12,19 @@ import java.util.Optional;
 public interface ReportRepository extends CrudRepository<Report,Integer> {
     // 보고서 갖고오기
     Optional<Report> findByReportId(Integer reportId);
-//    Optional<Report> findAllByFieldAndGradeAndIsAcceptedAndAccess_UserOrAccess_EveryOrderByCreatedAt(Field field, Grade grade, Integer isAccepted,Access access);
+
+    // 필터가 분야, 타입 모두 적용시 ORM
+    Page<Report> findAllByAccessAndFieldAndTypeAndGradeAndIsAcceptedTrueAndIsSubmittedTrue(Access access, Field field, Type type, Grade grade, Pageable page);
+
+    // 필터가 분야, 타입 모두 적용시 ORM
+    Page<Report> findAllByAccessAndGradeAndIsAcceptedTrueAndIsSubmittedTrue(Access access, Grade grade, Pageable page);
+
+    // 필터가 타입만 적용시 ORM
+    Page<Report> findAllByAccessAndTypeAndGradeAndIsAcceptedTrueAndIsSubmittedTrue(Access access, Type type,Grade grade, Pageable page);
+
+    // 필터가 분야만 적용시 ORM
+    Page<Report> findAllByAccessAndFieldAndGradeAndIsAcceptedTrueAndIsSubmittedTrue(Access access, Field field,Grade grade, Pageable page);
+
+    //제목 검색 ORM
+    Page<Report> findAllByAccessAndIsAcceptedTrueAndIsSubmittedTrueAndTitleContaining(Access access, String title, Pageable page);
 }
