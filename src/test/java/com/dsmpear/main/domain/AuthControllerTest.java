4942f91d459fddf77d6fa9a1183edf21b23e6fc6
kjbin0420 <kjbin0420@naver.com>
2020-12-14 13:44:45 +0900

    [TEST] test codes

--- src/test/java/com/dsmpear/main/domain/AuthControllerTest.java
+++ src/test/java/com/dsmpear/main/domain/AuthControllerTest.java
@@ -75,6 +75,7 @@ public class AuthControllerTest {
     public void exit() {
         userRepository.deleteAll();
         tokenRepository.deleteAll();
+        
         refreshTokenRepository.deleteAll();
     }
 
