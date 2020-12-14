9755ca6d22fb2538a10ee0bf7f53a7f223efae8a
Hong Jung hyeon <jhhong0509@naver.com>
2020-12-14 18:50:30 +0900

    fix report test

--- src/main/java/com/dsmpear/main/entity/userreport/UserReportRepository.java
+++ src/main/java/com/dsmpear/main/entity/userreport/UserReportRepository.java
@@ -5,7 +5,10 @@ import com.dsmpear.main.entity.report.Report;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserReportRepository extends CrudRepository<UserReport,Integer> {
     List<UserReport> findAllByUserEmail(String email);
+
+    Optional<UserReport> findByReportId(Integer reportId);
 }
