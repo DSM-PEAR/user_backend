870ddc87b9ac3114a863a0fcb849f8d21fbbbf61
승윤 <64132926+syxxn@users.noreply.github.com>
2020-12-14 18:02:54 +0900

    Revert "Merge branch 'profile' into develop"
    This reverts commit 8de29523419be932540f8504b9c1371949590e1a.

--- src/main/java/com/dsmpear/main/entity/report/ReportRepository.java
+++ src/main/java/com/dsmpear/main/entity/report/ReportRepository.java
@@ -1,5 +1,7 @@
 package com.dsmpear.main.entity.report;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
