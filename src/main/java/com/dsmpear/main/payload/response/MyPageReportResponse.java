d5d69a6a9fefc6ef9f157d7b50529136b875deb3
Hong Jung hyeon <jhhong0509@naver.com>
2020-12-17 18:24:07 +0900

    wtff

--- src/main/java/com/dsmpear/main/payload/response/MyPageReportResponse.java
+++ src/main/java/com/dsmpear/main/payload/response/MyPageReportResponse.java
@@ -1,22 +1,2 @@
-package com.dsmpear.main.payload.response;
-
-import lombok.Builder;
-import lombok.Getter;
-
-import java.time.LocalDateTime;
-
-@Getter
-@Builder
-public class MyPageReportResponse {
-
-    private Integer reportId;
-
-    private String title;
-
-    private LocalDateTime createdAt;
-
-    private Boolean isSubmitted; // 임시저장 or 제출
-
-    private Boolean isAccepted; // 승인 or 미승인
-
+package com.dsmpear.main.payload.response;public class MyPageReportResponse {
 }
