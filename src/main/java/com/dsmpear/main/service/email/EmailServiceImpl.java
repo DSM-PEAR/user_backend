5a9e5f029cb5029061dc3760bab63c4e87b8c08b
kjbin0420 <kjbin0420@naver.com>
2020-12-28 08:35:54 +0900

    [Fix] Email Logic

--- src/main/java/com/dsmpear/main/service/email/EmailServiceImpl.java
+++ src/main/java/com/dsmpear/main/service/email/EmailServiceImpl.java
@@ -7,10 +7,6 @@ import com.dsmpear.main.exceptions.SecretKeyNotMatchedException;
 import com.dsmpear.main.payload.request.NotificationRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import net.sargue.mailgun.Configuration;
-import net.sargue.mailgun.Mail;
-import net.sargue.mailgun.Response;
-import net.sargue.mailgun.content.Body;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.annotation.Async;
@@ -28,12 +24,6 @@ import java.util.Random;
 public class EmailServiceImpl implements EmailService {
     private final PasswordEncoder passwordEncoder;
     private final VerifyNumberRepository numberRepository;
-
-    @Value("${email.domain}")
-    private String domain;
-
-    @Value("${email.apiKey}")
-    private String apiKey;
 
     @Value("${server.url}")
     private String url;
@@ -54,52 +44,14 @@ public class EmailServiceImpl implements EmailService {
         sendAuthNumEmail(sendTo);
     }
 
+    @Async
     public void sendNotificationEmail(NotificationRequest request) {
-        Configuration configuration = new Configuration()
-                .domain(domain)
-                .apiKey(apiKey)
-                .from("PEAR", "pear@dsm.hs.kr");
 
-        Body builder = new Body(convertNotificationHtmlWithString(request), "");
-
-        Response response = Mail.using(configuration)
-                .to(request.getEmail())
-                .subject("[PEAR] 승인 알림")
-                .content(builder)
-                .build()
-                .send();
-
-        if (!response.isOk())
-            throw new EmailSendFailedException();
     }
 
     @Async
     public void sendAuthNumEmail(String sendTo) {
-        String number = generateVerifyNumber();
 
-        Configuration configuration = new Configuration()
-                .domain(domain)
-                .apiKey(apiKey)
-                .from("PEAR", "pear@dsm.hs.kr");
-
-        Body builder = new Body(convertNumberHtmlWithString(number), "");
-
-        numberRepository.save(
-                VerifyNumber.builder()
-                        .email(sendTo)
-                        .verifyNumber(number)
-                        .build()
-        );
-
-        Response response = Mail.using(configuration)
-                .to(sendTo)
-                .subject("[PEAR] 인증번호 알림")
-                .content(builder)
-                .build()
-                .send();
-
-        if (!response.isOk())
-            throw new EmailSendFailedException();
     }
 
     private String generateVerifyNumber() {
