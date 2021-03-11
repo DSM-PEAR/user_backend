a4d22a33a1f7b726db00486d9b750e430fdb44ca
이승윤 <yyuunn17@naver.com>
2021-03-11 16:17:23 +0900

    [FIX] 멤버리스트 반환형 수정

--- src/main/java/com/dsmpear/main/entity/member/MemberRepository.java
+++ src/main/java/com/dsmpear/main/entity/member/MemberRepository.java
@@ -1,7 +1,6 @@
 package com.dsmpear.main.entity.member;
 
 import com.dsmpear.main.entity.report.Report;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -11,7 +10,7 @@ import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends CrudRepository<Member,Integer> {
-    Page<Member> findAllByReport(Report report, Pageable page);
+    List<Member> findAllByReport(Report report, Pageable page);
     Optional<Member> findByReportAndUserEmail(Report report, String userEmail);
     void deleteAllByReport(Report report);
 }
