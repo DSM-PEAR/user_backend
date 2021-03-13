93d88f10f03ed0c251fe14a0f5183b4048dd02d6
이승윤 <yyuunn17@naver.com>
2021-03-13 15:05:54 +0900

    [UPDATE] page 파라미터 없애고 list로 주기

--- src/main/java/com/dsmpear/main/service/user/UserListService.java
+++ src/main/java/com/dsmpear/main/service/user/UserListService.java
@@ -4,5 +4,5 @@ import com.dsmpear.main.payload.response.UserListResponse;
 import org.springframework.data.domain.Pageable;
 
 public interface UserListService {
-    UserListResponse getUserList(String name, Pageable page);
+    UserListResponse getUserList(String name);
 }
