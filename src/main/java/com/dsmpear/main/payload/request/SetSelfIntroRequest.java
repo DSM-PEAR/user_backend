0a5a91740dc75685eb42b15e60808333a5b81001
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-04-16 09:43:49 +0900

    [FIX] myPageServiceImpl

--- src/main/java/com/dsmpear/main/payload/request/SetSelfIntroRequest.java
+++ src/main/java/com/dsmpear/main/payload/request/SetSelfIntroRequest.java
@@ -1,10 +1,14 @@
 package com.dsmpear.main.payload.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SetSelfIntroRequest {
 
     private String intro;
