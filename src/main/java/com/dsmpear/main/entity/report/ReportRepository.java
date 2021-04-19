95e389557212463ef971d6795736c70c89f8a1a5
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-04-19 23:03:30 +0900

    [DELETE] @Query

--- src/main/java/com/dsmpear/main/entity/report/ReportRepository.java
+++ src/main/java/com/dsmpear/main/entity/report/ReportRepository.java
@@ -16,10 +16,6 @@ public interface ReportRepository extends CrudRepository<Report,Integer> {
     // 보고서 갖고오기
     Optional<Report> findById(Integer reportId);
 
-    // 도전!
-    @Query("SELECT a FROM Report a WHERE a.isAccepted = true AND a.isSubmitted = true AND a.access = :access AND a.grade = :grade AND (:field IS NULL OR a.field = :field) AND (:type IS NULL OR a.type = :type)")
-    Page<Report> findAllByAccessAndGradeAndFieldAndType(@Param("access") Access access, @Param("grade") Grade grade, @Param("field") Field field, @Param("type") Type type, Pageable page);
-
     //제목 검색 ORM
     Page<Report> findAllByAccessAndIsAcceptedTrueAndIsSubmittedTrueAndTitleContainsOrderByCreatedAtDesc(Access access, String title, Pageable page);
 }
